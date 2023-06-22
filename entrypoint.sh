184ec31630357440c5866d503a8e341f957fd3c2
ashnamehrotra <ashnamehrotra@gmail.com>
2023-06-22 23:08:11 +0000

    use
    Signed-off-by: ashnamehrotra <ashnamehrotra@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -l
+#!/bin/sh
 
 image=$1
 report=$2
@@ -15,7 +15,9 @@ new_tag="$old_tag$patched_tag"
 if copa patch -i "$image" -r /data/"$report" -t "$new_tag" --addr tcp://0.0.0.0:8888;
 then
     patched_image="$image_no_tag:$new_tag"
-    echo "::set-output name=patched-image::$patched_image"
+    # echo "::set-output name=patched-image::$patched_image"
+    echo "patched-image=$patched_image" >> $GITHUB_OUTPUT
 else
     echo "Error patching image $image with copa"
+    exit 1
 fi
