c7396d57508992b734492afbfcd4e0e1806ab6ed
ashnamehrotra <ashnamehrotra@gmail.com>
2024-02-08 14:25:29 -0800

    enable support for buildx
    Signed-off-by: ashnamehrotra <ashnamehrotra@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ patched_tag=$3
 timeout=$4
 output_file=$5
 format=$6
-
+buildkitd_container=$7
 
 # parse image into image name
 image_no_tag=$(echo "$image" | cut -d':' -f1)
@@ -19,8 +19,18 @@ else
     output="--format $format --output ./data/"$output_file""
 fi
 
+# check if buildkit container is set
+if [ -z "$buildkitd_container" ]
+then
+    docker buildx create --name=copa-action
+    docker buildx use --default copa-action
+    buildkit="--addr buildx://copa-action"
+else
+    buildkit="--addr tcp://127.0.0.1:8888"
+fi
+
 # run copa to patch image
-if copa patch -i "$image" -r ./data/"$report" -t "$patched_tag" --addr tcp://127.0.0.1:8888 --timeout $timeout $output;
+if copa patch -i "$image" -r ./data/"$report" -t "$patched_tag" $buildkit --timeout $timeout $output;
 then
     patched_image="$image_no_tag:$patched_tag"
     echo "patched-image=$patched_image" >> "$GITHUB_OUTPUT"
@@ -28,4 +38,3 @@ else
     echo "Error patching image $image with copa"
     exit 1
 fi
-
